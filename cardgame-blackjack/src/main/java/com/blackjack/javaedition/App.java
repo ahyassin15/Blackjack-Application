48a225cdb8fcee220cea3689702b7ea6a38ead01
Ahmed <ah.yassin05@gmail.com>
2025-01-27 17:44:10 -0500

    initial commit

---
+++ cardgame-blackjack/src/main/java/com/blackjack/javaedition/App.java
@@ -0,0 +1,10 @@
+package com.blackjack.javaedition;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
